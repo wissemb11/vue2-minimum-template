6d70b467b21b1eb74f27a1327e545ed6bfba8f8d
wissem <wissemb11@gmail.com>
2022-10-02 21:03:08 +0100

    .

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>footer component</footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+footer {
+  position: fixed;
+  width: 100%;
+  padding: 10px;
+  bottom: 0;
+  left: 0;
+  background: #fff;
+  border-top: 1px solid #eee;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>Hello world component</div>
+
+
+
+</template>
+
+
+
+<script>
+export default {
+
+  name: "Navigation",
+  methods: {
+    getCountryFlagURL(locale) {
+      // Map locales to country codes to use with Country Flags API
+      const map = {
+        en: "us",
+        cs: "cz",
+      };
+      return `https://flagcdn.com/24x18/${map[locale]}.png`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  margin-bottom: 60px;
+}
+</style>
